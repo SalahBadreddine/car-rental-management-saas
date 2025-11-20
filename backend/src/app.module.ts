72df3d5a1445a10074a68abb0e827964d4903ab2
Badro <badro.zerraf05@gmail.com>
2025-11-20 23:43:05 +0100

    enhanved login backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller'; 
 import { AppService } from './app.service';     
 import { AuthModule } from './modules/auth/auth.module'; 
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller'; 
+import { AppService } from './app.service';     
+import { AuthModule } from './modules/auth/auth.module'; 
 
 @Module({
   imports: [
