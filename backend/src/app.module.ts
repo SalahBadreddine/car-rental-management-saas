ac876a4b34d81b2018e8eb7af37104eb7d942401
Badro <badro.zerraf05@gmail.com>
2025-11-20 23:43:23 +0100

    modified the app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller'; 
 import { AppService } from './app.service';     
 import { AuthModule } from './modules/auth/auth.module'; 
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller'; 
-import { AppService } from './app.service';     
-import { AuthModule } from './modules/auth/auth.module'; 
 
 @Module({
   imports: [
