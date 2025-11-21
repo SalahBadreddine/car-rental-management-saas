651387898e833a3e0b3ab8e5e54637d884966377
Badro <badro.zerraf05@gmail.com>
2025-11-21 17:52:11 +0100

    implemented Location Api Module with Storage Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller'; 
 import { AppService } from './app.service';     
 import { AuthModule } from './modules/auth/auth.module'; 
+import { LocationsModule } from './modules/locations/locations.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+    LocationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
